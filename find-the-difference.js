c61c2c6530c7407d559e6152d00b9519e445217d
Wes White <wtealwhite@gmail.com>
2019-01-04 12:49:04 -0600

    cleaned up runtime by removing index of call in filter

--- find-the-difference.js
+++ find-the-difference.js
@@ -5,15 +5,21 @@
  * @param {string} t
  * @return {character}
  */
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {character}
+ */
 var findTheDifference = function(s, t) {
     let sValues = s.split("")
     let tValues = t.split("")
     
     return tValues.filter( v => {
-        if(sValues.indexOf(v) === -1){
+        index = sValues.indexOf(v) 
+        if(index === -1){
             return true
         } else {
-            sValues.splice(sValues.indexOf(v), 1)
+            sValues.splice(index, 1)
             return false
         }
     })[0]
