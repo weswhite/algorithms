96ff4903537bb0565296c6dd79998c087894364d
Wes White <wtealwhite@gmail.com>
2019-01-15 22:41:02 -0600

    Update string-to-integer-atoi.js

--- string-to-integer-atoi.js
+++ string-to-integer-atoi.js
@@ -5,16 +5,36 @@
  * @return {number}
  */
 var myAtoi = function(str) {
-    const splitStr = str.split(" ").filter(s => {
-        if(s == "") return false
-        else return true
-    })
-    console.log(splitStr)
-    if(parseInt(splitStr[0])){
-        //check bounds
-        if(splitStr[0] > 2147483648) return 2147483648
-        else if(splitStr[0] < -2147483648) return -2147483648
-        else return splitStr[0]
-        //if overflow retrun max
-    } else return 0
+    let i = 0;
+    let res = 0;
+    let isNegative = false;
+    
+    // 1. Skip spaces
+    while (str[i] === ' ') {
+        i += 1;
+    }
+    
+    // 2. Optional +/-
+    const maybeSign = str[i];
+    if (maybeSign === '+' || maybeSign === '-') {
+        isNegative = maybeSign === '-';
+        i += 1;
+    }
+
+    // 3. Process numbers and stop once an invalid character is found
+    for (; i < str.length; i += 1) {
+        const code = str.charCodeAt(i) - 48; // '0' is 48
+        if (code < 0 || code > 9) {
+            break;
+        }
+        res *= 10;
+        res += code;
+    }
+    
+    if (isNegative) {
+        res = -res;
+    }
+    
+    return Math.max(-(2**31), Math.min(2**31 - 1, res));
 };
+
