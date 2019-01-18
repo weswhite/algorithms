43a230e20d8f2c41cbb72f30777b78dd96fc5723
Wes White <wtealwhite@gmail.com>
2019-01-17 20:49:26 -0600

    fix 1.4

--- 1.4.js
+++ 1.4.js
@@ -1,9 +1,18 @@
-const pal = (s1, s2) => {
-    const reverse1 = []
-    const ss = s1.split('')
-    for(i = 0; i < s1.length; i++){
-        reverse1.push(ss.pop())
-    }
-    if (reverse1.toString() == s2) return true
-    else return false
+// const pal = (s1, s2) => {
+//     const reverse1 = []
+//     const ss = s1.split('')
+//     for(i = 0; i < s1.length; i++){
+//         reverse1.push(ss.pop())
+//     }
+//     if (reverse1.toString() == s2) return true
+//     else return false
+// }
+
+const pal = (S) => {
+    const ss1 = S.split("")
+    const ss2 = [...ss1]
+    if(S.length % 2 !== 0) return false
+    const c1 = ss1.splice(0, S.length/2).join("")
+    const c2 = ss2.splice(S.length/2, S.length/2).reverse().join("")
+    if(c1 == c2) return true
 }
