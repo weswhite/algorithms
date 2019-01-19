f985cce6ee37d1517ad4d9514251298947ae22f4
Wes White <wtealwhite@gmail.com>
2019-01-18 18:47:37 -0600

    next greater elelemtn

---
+++ next-greater-element-1.js
@@ -0,0 +1,9 @@
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number[]}
+ */
+var nextGreaterElement = function(nums1, nums2) {    
+    return nums1.map((n) => nums2.slice(nums2.indexOf(n)).find(i => i > n) || -1);
+    
+};
